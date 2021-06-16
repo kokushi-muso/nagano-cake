9520123e402de8eb98d717667d58b90d39b37f7a
Masatooo0 <masato.n.0321@gmail.com>
2021-06-16 05:08:56 +0000

    [add]管理者設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,13 @@ class ApplicationController < ActionController::Base
     ])
   end
 
+  def after_sign_in_path_for(resource)
+    case resource
+    when Admin
+      admin_orders_path
+    when Customer
+      root_path
+    end
+  end
+
 end

b025859f46a3ca8d2cc5722cab61f54f3fd41b6a
masaya-kawamura <lucky.number0128@gmail.com>
2021-06-16 19:56:21 +0000

    コンフリクトを解消し、customersモデルのアソシエーションを追加しました

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,11 +1,15 @@
 class Customer < ApplicationRecord
+
   has_many :carts, dependent: :destroy
   has_many :orders, dependent: :destroy
   has_many :addresses, dependent: :destroy
   has_many :cart_items, through: :carts, source: :item
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
   # ======お客様登録時のバリデーション=======================
   with_options presence: true do
     validates :first_name

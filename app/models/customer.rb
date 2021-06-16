2369511836c84219e7c88d1f37d9d53db87e1e7e
masaya-kawamura <lucky.number0128@gmail.com>
2021-06-16 15:10:22 +0000

    [認証機能]カスタマーのログインとサインイン画面とバリデーションの設定、ログイン後ログアウト後の遷移先をせってしました。

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,17 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  # ======お客様登録時のバリデーション=======================
+  with_options presence: true do
+    validates :first_name
+    validates :last_name
+    validates :first_name_kana
+    validates :last_name_kana
+    validates :address
+    validates :email
+    validates :postcode
+    validates :phone_number
+  end
+        
 end

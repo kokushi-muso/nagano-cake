3df7168860291471af9063be73cae2d980b40bc2
masaya-kawamura <lucky.number0128@gmail.com>
2021-06-16 18:25:18 +0000

    [会員側]カスタマー編集ページを作成しました

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -5,8 +5,33 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = current_customer
   end
 
   def update
+    customer = Customer.find(params[:id])
+    if customer.update(customer_params)
+      flash[:notice] = '会員情報を編集しました'
+      redirect_to customers_mypage_path
+    else
+      @customer = customer
+      render :edit
+    end
   end
+  
+  
+    private
+    
+    def customer_params
+      params.require(:customer).permit(
+        :first_name,
+        :last_name,
+        :first_name_kana,
+        :last_name_kana,
+        :postcode,
+        :address,
+        :email,
+        :phone_number
+        )
+    end
 end

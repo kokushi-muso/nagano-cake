e1a04976b1c9b1a6757bf2db8c9905be5983596e
Masatooo0 <masato.n.0321@gmail.com>
2021-06-16 05:10:25 +0000

    [add]注文履歴ページ

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,13 @@
 class Admin::OrdersController < ApplicationController
+
+  before_action :authenticate_admin!
+
   def index
+    @orders = Order.all
   end
 
   def show
+    @order = Order.find(params[:id])
+    @order_items = @order.order_items
   end
 end

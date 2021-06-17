7004e34d6e78984c147814565a9a4983f1b146df
Masatooo0 <masato.n.0321@gmail.com>
2021-06-17 02:21:35 +0000

    [delete]管理者設定

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-#管理者設定
-Admin.create!(
-   email: 'admin@admin',
-   password: 'testtest'
-)

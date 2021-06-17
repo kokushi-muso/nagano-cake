1a28f6bfed9dd03513d4a180316bfcdad2961fcd
masaya-kawamura <lucky.number0128@gmail.com>
2021-06-17 06:37:34 +0000

    配送先登録画面とseedファイルにid:1のcustomerの情報を追加

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,7 +1,29 @@
 class Public::AddressesController < ApplicationController
+  
   def index
+    @address = Address.new
+    @addresses = current_customer.addresses
+  end
+  
+  def create
+    address = Address.new(address_params)
+    address.customer_id = current_customer.id
+    if address.save
+      redirect_to addresses_path(current_customer)
+    else
+      @address = address
+      @addresses = current_customer.addresses
+      render :index
+    end
   end
 
   def edit
   end
+  
+    private
+    
+    def address_params
+      params.require(:address).permit(:postcode, :address, :recieve_name)
+    end
+    
 end

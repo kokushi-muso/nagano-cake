252011e432e026e456ad3ee71700094d0c662c47
Masatooo0 <masato.n.0321@gmail.com>
2021-06-18 02:07:42 +0000

    [edit]注文履歴一覧

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,11 +3,12 @@ class Admin::OrdersController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @orders = Order.all
+    @orders = Order.page(params[:page]).per(10)
   end
 
   def show
-    @order = Order.find(1)  #params[:id]              
+    @order = Order.find(params[:id])  #params[:id]
     @order_items = @order.order_items
+    @shipping_fee = 800
   end
 end

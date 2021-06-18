0d7b8b802a9e2e376afc3dc6447bb01e643e4bd6
masaya-kawamura <lucky.number0128@gmail.com>
2021-06-18 20:43:09 +0000

    会員側の仮実装

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,11 @@
 class Cart < ApplicationRecord
   belongs_to :item
   belongs_to :customer
+  
+  with_options presence: true do
+    validates :item_id
+    validates :customer_id
+    validates :quantity
+  end
+  
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,4 +2,5 @@ class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :item
   enum product_status: [:着手不可, :製作待ち, :製作中, :制作完了]
+  
 end

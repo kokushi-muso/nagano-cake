5318fb7ec61cd25ba7288b6e5e99c9d45a0a8d76
Masatooo0 <masato.n.0321@gmail.com>
2021-06-18 07:50:14 +0000

    [add]ステータス変更

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -7,8 +7,25 @@ class Admin::OrdersController < ApplicationController
   end
 
   def show
-    @order = Order.find(params[:id])  #params[:id]
+    @order = Order.find(params[:id])
     @order_items = @order.order_items
-    @shipping_fee = 800
   end
+
+  def update
+    @order = Order.find(params[:id])
+    if @order.update(order_params)
+      flash[:success] = "注文ステータスを変更しました"
+      redirect_to admin_order_path(@order)
+    else
+      render "show"
+    end
+  end
+
+
+  private
+
+	def order_params
+		  params.require(:order).permit(:order_status)
+	end
+
 end

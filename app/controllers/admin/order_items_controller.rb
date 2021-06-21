409e73f5d97145269eec3b565a02f4df008b2916
Masatooo0 <masato.n.0321@gmail.com>
2021-06-21 08:43:12 +0000

    注文詳細画面名前リンク修正

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -2,7 +2,6 @@ class Admin::OrderItemsController < ApplicationController
   before_action :authenticate_admin!
 
   def update
-    @order_item.update(order_item_params)
     flash[:success] ="製作ステータスを変更しました"
     @order_item = OrderItem.find_by(order_id: params[:order_id], id: params[:id])
     imformation = @order_item.order
@@ -11,7 +10,7 @@ class Admin::OrderItemsController < ApplicationController
         imformation.update(order_status:2)
       end
       if imformation.order_items.all?{ |order_item| order_item.product_status == "製作完了"}
-        imformation.update(order_status:4)
+        imformation.update(order_status:3)
       end
       redirect_to admin_order_path(@order_item.order)
   end

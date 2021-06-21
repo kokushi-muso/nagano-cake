2cdeae1dbca0418a654c22a192cf3dae74af6c41
n-s0120 <satoshicuni@gmail.com>
2021-06-21 03:05:46 +0000

    topic4-0621

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,10 +3,9 @@ class Item < ApplicationRecord
   has_many :order_items
   belongs_to :genre
 
-  
+
   attachment :image
-  
-=======
+
   validates :name, presence: true
   validates :name, uniqueness: true
 end

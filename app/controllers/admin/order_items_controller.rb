11eca98dc10408e87c3df2e198f07c3b9a7cf10c
Masatooo0 <masato.n.0321@gmail.com>
2021-06-21 08:17:32 +0000

    testcommit

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -2,27 +2,24 @@ class Admin::OrderItemsController < ApplicationController
   before_action :authenticate_admin!
 
   def update
+    @order_item.update(order_item_params)
+    flash[:success] ="製作ステータスを変更しました"
     @order_item = OrderItem.find_by(order_id: params[:order_id], id: params[:id])
     imformation = @order_item.order
+
     if params[:order_item][:product_status] == "製作中"
       imformation.update(order_status:2)
     end
-    unless imformation.order_items.all?{ |order_item| order_item.product_status == "製作完了"}
+    if imformation.order_items.all?{ |order_item| order_item.product_status == "製作完了"}
       imformation.update(order_status:4)
     end
-    if @order_item.update(order_item_params)
-      flash[:success] ="製作ステータスを変更しました"
       redirect_to admin_order_path(@order_item.order)
-    else
-      render "show"
-    end
-
-
-    private
-
-    def order_item_params
-      params.require(:order_item).permit(:product_status)
-    end
-
   end
+
+  private
+
+  def order_item_params
+    params.require(:order_item).permit(:product_status)
+  end
+
 end

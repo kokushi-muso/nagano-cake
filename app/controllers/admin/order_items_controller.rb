12d7d723e795be268973d6413179fd33cd47f8a1
sakasegawajunki <j.sakasegawa18@gmail.com>
2021-06-22 00:11:07 +0000

    レイアウト修正

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -4,17 +4,20 @@ class Admin::OrderItemsController < ApplicationController
   def update
     @order_item = OrderItem.find_by(order_id: params[:order_id], id: params[:id])
     imformation = @order_item.order
-    @order_item.update(order_item_params)
-      if params[:order_item][:product_status] == "製作中"
-        imformation.update(order_status:2)
-      end
-      if imformation.order_items.all?{ |order_item| order_item.product_status == "製作完了"}
-        imformation.update(order_status:4)
-      end
+    if params[:order_item][:product_status] == "製作中"
+      imformation.update(order_status:2)
+    end
+    unless imformation.order_items.all?{ |order_item| order_item.product_status == "製作完了"}
+      imformation.update(order_status:3)
+    end
+    if @order_item.update(order_item_params)
+      flash[:success] ="製作ステータスを変更しました"
       redirect_to admin_order_path(@order_item.order)
+    else
+      render "show"
+    end
+
   end
-  
-  
   private
 
   def order_item_params
